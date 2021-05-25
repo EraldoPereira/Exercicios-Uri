c4554b2c55cb22868d74c2d9e89185b3b4524e21
Eraldo <eraldosimaopereira@gmail.com>
2021-05-24 21:07:51 -0300

    Adicionado o 1143

---
+++ uri1143.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var valor = parseInt(input.split('\n'))
+
+if( valor > 1 && valor < 1000){
+    for(let index = 1; index <= valor; index++ ){
+        console.log( (index), (index * index ), ( ( index * index ) * index ) );
+    }
+}
